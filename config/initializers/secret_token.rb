c3c51a0a9d60f3539f8b1dbad5f596a17d244857
Tiago Kaphan <tiagok@gmail.com>
2011-01-07 17:59:35 -0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tom5::Application.config.secret_token = '5bf79bb42746fe2c4f2a3b88bbbb34d72a329ee4a072954dd24bf33186b634ea623b231e55b4bf5fe9de2e36da0ded2b31452d591b8974ca3396ccf185b21fdc'
